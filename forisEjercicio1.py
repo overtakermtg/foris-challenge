2d5a59ce685bf02d1abe69bc0ba9cfe8f88b2e6d
overtakermtg <angel.overtaker@gmail.com>
2018-07-17 15:34:16 -0400

    Create forisEjercicio1.py

---
+++ forisEjercicio1.py
@@ -0,0 +1,12 @@
+# Ejercicio 1 foris
+text= "afoolishconsistencyisthehobgoblinoflittlemindsadoredbylittlestatesmenandphilosophersanddivineswithconsistencyagreatsoulhassimplynothingtodohemayaswellconcernhimselfwithhisshadowonthewallspeakwhatyouthinknowinhardwordsandtomorrowspeakwhattomorrowthinksinhardwordsagainthoughitcontradicteverythingyousaidtodayahsoyoushallbesuretobemisunderstoodisitsobadthentobemisunderstoodpythagoraswasmisunderstoodandsocratesandjesusandlutherandcopernicusandgalileoandnewtonandeverypureandwisespiritthatevertookfleshtobegreatistobemisunderstood"
+
+# recorre todos los largos posibles de textos
+for fin in range(0,len(text)):
+	#recorre todo el texto buscando cambiando el inicio de la palabra
+	for ini in range(0,len(text)):
+		# guarda la palabra generada para luego evaluarla
+		word = text[ini:fin]
+		if word == word[::-1] and len(word) > 3:
+			# solo imprime los palindromos 
+			print(word)
